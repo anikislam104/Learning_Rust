8cf6f6c91b54894a5308b86c6a8de6f4eb70c5f8
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2025-03-15 15:32:36 +0600

    learning variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let num:i8 = -8;
+    println!("{}", num);
+    println!("{}", num + 1);
+    println!("{}", num - 1);
+    // print!(num);
+    let num2:u32 = 1000;
+    let float_num:f32 = 3.14;
+    println!("num2: {}, float_num: {}", num2, float_num);
 }
